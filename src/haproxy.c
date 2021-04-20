a025fa96b8d520dd7470617e4bcb241ef8e5a112
Amaury Denoyelle <adenoyelle@haproxy.com>
2021-04-20 16:48:22 +0200

    BUG/MINOR: server: free srv.lb_nodes in free_server
    lb_nodes is allocated for servers using lb_chash (balance random or
    hash-type consistent).
    
    It can be backported up to 1.8.
    
    (cherry picked from commit fb247946a1b0a6dc6b1e6b27631adf1ef40c0ee3)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    [Cf: Patch applied on haproxy.c because servers are freed in deinit()]
    (cherry picked from commit 2bf658c0d55b73b175ac736d21aa866674a68a37)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit e61f1095e9bfdd24587fcb8bfe517b7da14d1f93)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/haproxy.c
+++ src/haproxy.c
@@ -2721,6 +2721,7 @@ void deinit(void)
 			free(s->safe_conns);
 			free(s->idle_orphan_conns);
 			free(s->curr_idle_thr);
+			free(s->lb_nodes);
 
 			if (s->use_ssl == 1 || s->check.use_ssl == 1 || (s->proxy->options & PR_O_TCPCHK_SSL)) {
 				if (xprt_get(XPRT_SSL) && xprt_get(XPRT_SSL)->destroy_srv)
