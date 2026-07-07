c4f20debbcca4ec1540b5b5a4bfc00dbb91750ee
Christopher Faulet <cfaulet@haproxy.com>
2026-07-07 09:03:46 +0200

    BUG/MINOR: stream: Fix custom timeouts initialization when setting backend
    Timeouts may be overwritten via the action "set-timeout". Client timeout is
    supported on frontends, connect/queue/tunnel/server timeouts are supported
    on backends and tarpit timeout is supported on both.
    
    However, there is an issue with the listener. In that case, the action is
    always executed in the frontend context and the backend-side timeouts are
    then reset, when the backend is selected. If it is another backend, it is
    logical. But most of time, the backend is unchanged. And in that case, these
    timeouts must be preserved.
    
    Documentation of set-timeout action was improved.
    
    This patch should be backported to all supported versions. Be carefull, all
    timeouts are not supported in 3.3 and lower (3.3->3.0: server/tunnel/client
    - 2.8->2.6: server/tunnel)
    
    (cherry picked from commit 69caa535300fce261dbab2d7fc3b841b882ae757)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit d545d3c963be346fab54a238fa821a52e8e47aea)
    [cf: Adapted as expected]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 62f1fcd6abe57d4eda7a250b341b547b34fe26f2)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 49051b0f9d83af4481887da69f7ec2bcf14c206d)
    [cf: Adapted to the context]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 26d9d085b954b2525a106c4a6e55f95c18b0fb19)
    [cf: Adapted to the context]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/stream.c
+++ src/stream.c
@@ -1119,11 +1119,15 @@ static int process_switching_rules(struct stream *s, struct channel *req, int an
 
 	}
 
-	/* we don't want to run the TCP or HTTP filters again if the backend has not changed */
 	if (fe == s->be) {
+		/* we don't want to run the TCP or HTTP filters again if the backend has not changed */
 		s->req.analysers &= ~AN_REQ_INSPECT_BE;
 		s->req.analysers &= ~AN_REQ_HTTP_PROCESS_BE;
 		s->req.analysers &= ~AN_REQ_FLT_START_BE;
+	}
+	else {
+		s->scb->ioto = TICK_ETERNITY;
+		s->tunnel_timeout = TICK_ETERNITY;
 	}
 
 	/* as soon as we know the backend, we must check if we have a matching forced or ignored
