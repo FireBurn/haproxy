9e5f279d662e29a1297a0867f0cb45bc0967b27f
Christopher Faulet <cfaulet@haproxy.com>
2026-07-07 10:07:31 +0200

    BUG/MINOR: stream: Fix custom max-retries initialization when setting backend
    It is possible to overwrite the configured max-retries value with the
    "set-retries" action. However there is an issue with the listeners. When the
    backend is set, this value is reset with the backend value. However, when
    the backend is unchanged, the custom value must be preserved.
    
    To fix the issue, when the stream is created, we set the max-retries to the
    listener value. It remains 0 for pure-frontend. And the backend value is
    used only if it is not the same proxy.
    
    The documentation of set-retries action was improved.
    
    This patch should be backported as far as 3.2.
    
    (cherry picked from commit b71d27e253fa6f8c82afd768f7691a4079f0f4f0)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 032d506881aa410ff46efccd46a183e1477bcc5d)
    [cf: context adjustment]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 63337e199b770e2a5232060148242c09307baeb7)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/stream.c
+++ src/stream.c
@@ -435,7 +435,7 @@ struct stream *stream_new(struct session *sess, struct stconn *sc, struct buffer
 	s->task = t;
 	s->pending_events = s->new_events = STRM_EVT_NONE;
 	s->conn_retries = 0;
-	s->max_retries = 0;
+	s->max_retries = ((sess->fe->cap & PR_CAP_BE) ? sess->fe->conn_retries : 0);
 	s->conn_exp = TICK_ETERNITY;
 	s->conn_err_type = STRM_ET_NONE;
 	s->prev_conn_state = SC_ST_INI;
@@ -1147,9 +1147,6 @@ static int process_switching_rules(struct stream *s, struct channel *req, int an
 
 	}
 
-	/* Se the max connection retries for the stream. may be overwritten later */
-	s->max_retries = s->be->conn_retries;
-
 	if (fe == s->be) {
 		/* we don't want to run the TCP or HTTP filters again if the backend has not changed */
 		s->req.analysers &= ~AN_REQ_INSPECT_BE;
@@ -1157,6 +1154,8 @@ static int process_switching_rules(struct stream *s, struct channel *req, int an
 		s->req.analysers &= ~AN_REQ_FLT_START_BE;
 	}
 	else {
+		/* Set the max connection retries for the stream. may be overwritten later */
+		s->max_retries = s->be->conn_retries;
 		s->scb->ioto = TICK_ETERNITY;
 		s->tunnel_timeout = TICK_ETERNITY;
 	}
